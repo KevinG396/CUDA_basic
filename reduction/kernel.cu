f70ece07a295c5cacbed7a1f7d1b5779387ed97c
KevinG396 <148742510+KevinG396@users.noreply.github.com>
2024-04-05 10:31:22 -0700

    Update kernel.cu

--- reduction/kernel.cu
+++ reduction/kernel.cu
@@ -10,7 +10,7 @@ __global__ void reduce0(int *g_idata, int *g_odata) {
     __syncthreads();
     // do reduction in shared mem
     for(unsigned int s=1; s < blockDim.x; s *= 2) {
-        if (tid % (2*s) == 0) {
+        if (tid % (2*s) == 0) { // will cause divergence, for the result of "if" is different
             sdata[tid] += sdata[tid + s];
         }
         //printf("res: %d\n", sdata[tid]);
