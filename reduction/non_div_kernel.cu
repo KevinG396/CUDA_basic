9c1f2d622a6c0cb6f7912103c147c33dfafabd56
KevinG396 <148742510+KevinG396@users.noreply.github.com>
2024-04-06 12:41:27 -0700

    Update non_div_kernel.cu

--- reduction/non_div_kernel.cu
+++ reduction/non_div_kernel.cu
@@ -10,6 +10,7 @@ __global__ void reduce_non_div(int *g_idata, int *g_odata) {
     __syncthreads();
     // do reduction in shared mem
     for(unsigned int s=1; s < blockDim.x; s *= 2) {
+        int index = 2 * s * tid;
         if (index < blockDim.x) {    // same result, non-divergence
         sdata[index] += sdata[index + s];
         }
